29448c75abb4bdb42f7936774f589d753bbab3f1
Rafael Garcia <TthanderBR@gmail.com>
2024-02-13 16:46:13 -0300

    Documentando...
    Documentado o modulo_SQL.py
    Adicionado detalhe de senha no README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # CLI_System
  Sistema CLI feito com InquirerPY_CLI
  
- 
+ __Usuario padrão:__ user / 1234
--- Source/Modules/module_SQL.py
+++ Source/Modules/module_SQL.py
@@ -3,24 +3,31 @@ from Librarys import(
 	Lib_Inquirer as inq
 )
 
+# Modificando a class Data_base da Lib_SQLite3 para utilizar Inquirer
 
 
 class Sql_Data_Base(sql.Data_base):
 	def __init__(self, file_name: str) -> bool:
+		# Classe herda a classe Data_base
 		super().__init__(file_name)
+		
+		# Verifica de o arquivo do banco existe
 		if self.exist:
+			# Se existir, apenas conecta ao ele
 			self.connect()
 		else:
+			# Senão, questiona ao usuario se deseja recriar o arquivo
 			if inq.confirm(
 				"Banco de dados não existente.\n  Deseja recrialo?",
 				qmark = "x",
 				style = {"questionmark" : "#ff0000"}
 			):	
-				# Create the data base
+				# Cria o arquivo do banco de dados e já conecta com ele
 				self.connect()
-				# Create the tables
+				# Cria a tabela de Usuarios para o sistema de login
+				# Tabela Users: [ Nome de usuario, senha, nivel de permição de acesso ]
 				self.create_table("Users", ["user_name", "password", "level"])
-				# Insert the default data
+				# Insere um usuario root e um usuario padrão para o acesso
 				self.insert("Users", ["root", "masterqi", 0])
-				self.insert("Users", ["hero", "3232", 1])
+				self.insert("Users", ["user", "1234", 3])
 
