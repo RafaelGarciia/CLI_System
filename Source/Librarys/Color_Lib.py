e74532bf5546051badf9762690c728826bd26eb4
Rafael Garcia <TthanderBR@gmail.com>
2024-06-10 09:46:33 -0300

    Removed class
    Removed the variables and functions from the class and passed directly to the root

--- Source/Librarys/Color_Lib.py
+++ Source/Librarys/Color_Lib.py
@@ -1,27 +1,26 @@
 from colorist import ColorRGB ,BgColorRGB
+#https://rgbcolorpicker.com
+
+clear = "\033[0m"
+
+red     : ColorRGB = ColorRGB(255, 000, 000)
+green   : ColorRGB = ColorRGB(000, 255, 000)
+blue    : ColorRGB = ColorRGB(000, 000, 255)
+
+black   : ColorRGB = ColorRGB(000, 000, 000)
+white   : ColorRGB = ColorRGB(255, 255, 255)
+
+yellow  : ColorRGB = ColorRGB(255, 255, 000)
+cyan    : ColorRGB = ColorRGB(000, 255, 255)
+pink    : ColorRGB = ColorRGB(255, 000, 255)
+
+orange  : ColorRGB = ColorRGB(255, 127, 000)
+purple  : ColorRGB = ColorRGB(127, 000, 255)
 
 
-class Color():
-    clear = "\033[0m"
 
-    red     : ColorRGB = ColorRGB(255, 000, 000)
-    green   : ColorRGB = ColorRGB(000, 255, 000)
-    blue    : ColorRGB = ColorRGB(000, 000, 255)
-
-    black   : ColorRGB = ColorRGB(000, 000, 000)
-    white   : ColorRGB = ColorRGB(255, 255, 255)
-
-    yellow  : ColorRGB = ColorRGB(255, 255, 000)
-    cyan    : ColorRGB = ColorRGB(000, 255, 255)
-    pink    : ColorRGB = ColorRGB(255, 000, 255)
-
-    orange  : ColorRGB = ColorRGB(255, 127, 000)
-    purple  : ColorRGB = ColorRGB(127, 000, 255)
-    #https://rgbcolorpicker.com
-
-
-    def RGB(self, red: int, green: int, blue: int) -> None:
-        return ColorRGB(red, green, blue)
+def RGB(self, red: int, green: int, blue: int) -> None:
+    return ColorRGB(red, green, blue)
 
 
 
